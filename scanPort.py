2d857613a1f46d32b3db0e094e22f6715ccc6b9e
星光下的逗比 <92913750+xzldb@users.noreply.github.com>
2023-01-04 23:51:40 +0800

    注释增加和部分优化

--- scanPort.py
+++ scanPort.py
@@ -1,28 +1,48 @@
 import socket
 import sys
+import threading
+import time
 
-def scanPort():
-    count = 0  # 发现端口的数量
-    for port in range(int(port_start), int(port_end) + 1):
-        sk = socket.socket()
-        sk.settimeout(0.1)  # 等待端口响应时间
-        conn_result = sk.connect_ex((host, port))  # 返回值，若为0则为端口开放
-        if conn_result == 0:
-            print('服务器{}的{}端口已开放'.format(host, port))
-            count += 1
-            print(sk.getsockname())
-        sk.close()
-    if count == 0:
-        print(f'服务器{host}的{ports}端口均未开放')
-        print("查询完毕")
-    sys.exit()
+threads = []  # 线程池
+thread_max = threading.BoundedSemaphore(65535)  # 最大信号量
+# 发现端口的数量
+count = 0
+
+#  扫描端口模块
+def scanPort(port, count=0):
+    sk = socket.socket()
+    sk.settimeout(0.1)  # 等待端口响应时间
+    int(port)
+    conn_result = sk.connect_ex((host, port))  # 尝试访问连接然后保存返回值，若为0则为端口开放
+    if conn_result == 0:
+        print('服务器{}的{}端口已开放'.format(host, port))
+        count += 1
+        # print(sk.getsockname()) #显示套接字自己的地址
+    sk.close()
+
+#  多线程
+def main(port):
+    for i in range(int(port_end) - int(port_start)):
+        thread_max.acquire()
+        t = threading.Thread(target=scanPort, args=(port,))
+        # 创建新线程执行scanPort，同时赋值ip=new_port
+        threads.append(t)
+        t.start()
+        port = int(port) + 1
+    for t in threads:
+        t.join()
 
 
 host = input('请输入服务器ip地址:')
-ports = input('请输入要扫描的端口范围，格式0-65536:')
+ports = input('请输入要扫描的端口范围，格式0-65535:')
+start = time.time()
 port_start, port_end = ports.split('-')  # 以’-‘为分割符
 h, o, s, t = host.split(".")
-if not 65535 > int(port_end) > int(port_start) >= 0 or not 255 > int(h) > 0 or not 255 > int(o) > 0 or not 255 > int(s) > 0 or not 255 > int(t) > 0:
+if not 65535 >= int(port_end) > int(port_start) >= 0 or not 255 > int(h) > 0 or not 255 > int(o) > 0 or not 255 > int(
+        s) > 0 or not 255 > int(t) > 0:
     print("ip地址有误，请重新输入")
     sys.exit()
-scanPort()
+main(port_start)
+end = time.time()
+print("------------耗时{0:.5f}秒------------".format(end - start))
+# 能跑但是会爆一堆红字，不知道为啥，先写其他的去了
