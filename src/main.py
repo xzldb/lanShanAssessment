450418dc8be24e19a938c0ae72dc9a71fa11147c
星光下的逗比 <92913750+xzldb@users.noreply.github.com>
2023-01-14 20:18:30 +0800

    项目增加

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 import scanPort
 import hostScanning
+import webtitlescanning
 
-target = input("请输入来选择功能：1=主机发现及操作系统识别，2=端口扫描及端口服务识别：")
+target = input("请输入来选择功能：1=主机发现及操作系统识别，2=端口扫描及端口服务识别,3=webtitle探测：")
 if int(target) == 1:
     hostScanning.begin()
 elif int(target) == 2:
     scanPort.begin()
+elif int(target) == 3:
+    webtitlescanning.begin()
 else:
     print('输入有误，请重新输入')
