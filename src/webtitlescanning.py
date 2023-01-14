a18ada0015d1f5cec3d3ec36fdba79837efa5e30
星光下的逗比 <92913750+xzldb@users.noreply.github.com>
2023-01-14 20:17:56 +0800

    webtitle探测

--- src/webtitlescanning.py
+++ src/webtitlescanning.py
@@ -1,4 +1,22 @@
-from urllib.request import urlopen
+import re
+import urllib.request
 
-myURL = urlopen("https://www.baidu.com/")
-print(myURL.read())
+def begin():
+    url = input("请输入探测的网址(需要包含http或https)：")
+    response = urllib.request.urlopen(url)
+    html = response.read()
+    html = html.decode()
+    tag = re.search(r'<title>(.*?)</title>', html).group(0)
+    tag = tag[:-8]
+    tag = tag[7:]
+    print(tag)
+
+def gettitletest():
+    url = 'https://www.bilibili.com/'
+    response = urllib.request.urlopen(url)
+    html = response.read()
+    html = html.decode()
+    tag = re.search(r'<title>(.*?)</title>', html).group(0)
+    tag = tag[:-8]
+    tag = tag[7:]
+    print(tag)
